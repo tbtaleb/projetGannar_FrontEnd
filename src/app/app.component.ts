c52630af0749bb67203dc963812d8a3c66956b34
unknown <ghzalayassin@gmail.com>
2024-10-07 22:29:21 +0100

    added candidate recommendations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RecommendedCandidatesComponent } from './components/recommendedCandidatesPage/recommended-candidates/recommended-candidates.component';
+import { JobDetailsComponent } from './components/recommendedCandidatesPage/job-details/job-details.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RecommendedCandidatesComponent,JobDetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
