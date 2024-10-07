0d58ed8f42f679bdc0a68dce87364c52db565d6b
Taysir <taysirbouzidi123@gmail.com>
2024-10-07 23:46:06 +0100

    userstory12_design_done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatchesComponentComponent } from './components/matches_component/matches_component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatchesComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
