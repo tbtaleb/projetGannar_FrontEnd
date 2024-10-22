089529551aaf14b1f60a44d08f5882ea9e4724bb
tbtaleb* <tbfeeber@gmail.com>
2024-10-22 11:11:59 +0100

    notifications

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { AppNotificationsComponent } from '../RecruiterNotificationComp/app-notifications/app-notifications.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']  // Corrected this line
 })
 export class NavbarComponent {
+  constructor(private dialog: MatDialog, private router:Router) {}
 
+  openNotificationDialog() {
+    this.router.navigate(['notifications']);
+  }
 }
