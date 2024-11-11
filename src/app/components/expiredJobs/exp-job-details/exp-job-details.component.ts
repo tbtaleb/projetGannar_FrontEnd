1fb1784b3b90289c077fce04c60578b3bf1858d2
MeDjb10 <131858818+MeDjb10@users.noreply.github.com>
2024-11-11 19:12:40 +0100

    Candidates order by score in expired job details

--- src/app/components/expiredJobs/exp-job-details/exp-job-details.component.ts
+++ src/app/components/expiredJobs/exp-job-details/exp-job-details.component.ts
@@ -14,7 +14,7 @@ export class ExpJobDetailsComponent implements OnInit {
   @Input() jobOfferId!: number;
   jobOffer: any = {};
   jobApplicants: any[] = [];
-  applicationScores: Map<number, number> = new Map();
+  applicationScores: Map<number, number> = new Map<number, number>();
 
   constructor(
     private jobOfferService: JobOfferService,
@@ -26,16 +26,14 @@ export class ExpJobDetailsComponent implements OnInit {
     this.getApplicantsByJobOfferId(this.jobOfferId);
   }
 
-  getApplicantsByJobOfferId(jobOfferId: number) {
+  getApplicantsByJobOfferId(jobOfferId: number): void {
     this.jobOfferService
       .getAllApplicantsByJobOfferId(jobOfferId)
       .subscribe((data) => {
         this.jobApplicants = data;
-        
-
-        // Fetch score for each applicant
-        this.jobApplicants.forEach((candidate) => {
-          this.getApplicationScore(candidate.id);
+        console.log(this.jobApplicants);
+        this.jobApplicants.forEach((applicant) => {
+          this.getApplicationScore(applicant.id);
         });
       });
   }
@@ -45,14 +43,13 @@ export class ExpJobDetailsComponent implements OnInit {
       .getApplicationByCandidateIdAndJobOfferId(candidateId, this.jobOfferId)
       .subscribe(
         (application: any) => {
-           if (application) {
-              console.log('Application:', application);
-              
-             const score = application.candidate_score;
-             console.log('Application score:', score);
-             
-             this.applicationScores.set(candidateId, score);
-           }
+          if (application) {
+            console.log('Application:', application);
+            const score = application.candidate_score;
+            console.log('Application score:', score);
+            this.applicationScores.set(candidateId, score);
+            this.sortApplicantsByScore();
+          }
         },
         (error) => {
           console.error('Error fetching application score:', error);
@@ -60,10 +57,18 @@ export class ExpJobDetailsComponent implements OnInit {
       );
   }
 
-  getJobOfferById(jobOfferId: number) {
+  sortApplicantsByScore(): void {
+    this.jobApplicants.sort((a, b) => {
+      const scoreA = this.applicationScores.get(a.id) || 0;
+      const scoreB = this.applicationScores.get(b.id) || 0;
+      return scoreB - scoreA;
+    });
+  }
+
+  getJobOfferById(jobOfferId: number): void {
     this.jobOfferService.getJobOfferById(jobOfferId).subscribe((data) => {
       this.jobOffer = data;
-      
+      console.log(this.jobOffer);
     });
   }
 }
